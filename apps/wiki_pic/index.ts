29e27b5dce2dccfe04ef0c826d0e6157fe2095c4
Sam Bocharov <sammbocharov@gmail.com>
2022-06-24 21:43:49 +0200

    big resolution link and config for .env file

--- apps/wiki_pic/index.ts
+++ apps/wiki_pic/index.ts
@@ -1,9 +1,14 @@
+import path from 'path';
 import createBot from '../../services/telegram';
 import getPotdInfo from '../../services/wiki_parser';
 
-require('dotenv').config();
+require('dotenv').config({ path: path.join(__dirname, '../../.env') });
 
 const bot = createBot(process.env.TELEGRAM_TOKEN as string);
+
+function getBigResolutionLink(link: string): string {
+  return link.replace('500px', '2560px');
+}
 
 function formatMessage(potd: any): string {
   return `*${potd.description}*\n`;
@@ -12,5 +17,5 @@ function formatMessage(potd: any): string {
 export default async function run() {
   const potd = await getPotdInfo();
   await bot.telegram.sendMessage(process.env.TELEGRAM_CHAT_ID as string, formatMessage(potd), { parse_mode: 'Markdown' });
-  bot.telegram.sendPhoto(process.env.TELEGRAM_CHAT_ID as string, potd.link);
+  bot.telegram.sendPhoto(process.env.TELEGRAM_CHAT_ID as string, getBigResolutionLink(potd.link));
 }
