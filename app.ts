b080946dbf4674bc1f2b7bba9e19554bb8bd0203
Sam Bocharov <semen_bard@mail.ru>
2022-08-08 17:57:45 +0200

    temp remove reminders

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import * as cron from 'node-cron';
 import bot from './entities/telegram';
 import PressureAction from './actions/pressure';
 import { checkLastMatch } from './apps/dotamatches/index';
-import { createReminder } from './apps/telegram/reminders';
+// import { createReminder } from './apps/telegram/reminders';
 import getPotdInfo from './apps/wiki-pic';
 
 require('dotenv').config({ path: path.join(__dirname, '.env') });
@@ -14,12 +14,12 @@ cron.schedule('* * * * *', () => {
 
 (async () => {
   const FIVE_MINS = 1000 * 60 * 5;
-  const HOUR = 1000 * 60 * 60;
+  // const HOUR = 1000 * 60 * 60;
 
-  const stretchHands = await createReminder(process.env.TG_W as string, process.env.STRETCH_MESSAGES?.split('*') as string[]);
-  setInterval(stretchHands, HOUR * 3);
-  const secretReminder = await createReminder(process.env.TG_W as string, process.env.SECRET_MESSAGES?.split('*') as string[]);
-  setInterval(secretReminder, HOUR * 4.5);
+  // const stretchHands = await createReminder(process.env.TG_W as string, process.env.STRETCH_MESSAGES?.split('*') as string[]);
+  // setInterval(stretchHands, HOUR * 3);
+  // const secretReminder = await createReminder(process.env.TG_W as string, process.env.SECRET_MESSAGES?.split('*') as string[]);
+  // setInterval(secretReminder, HOUR * 4.5);
   setInterval(checkLastMatch, FIVE_MINS);
   checkLastMatch();
 
